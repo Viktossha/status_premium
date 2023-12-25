39f842c4a15c2f83af97ea5b12bcb1a347a0c36b
user <vikapetrova8@gmail.com>
2023-12-25 08:58:31 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'main.js',
-        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+        assetModuleFilename: path.join('images', '[name].[ext]'),
     },
     module: {
         rules: [
@@ -36,15 +36,11 @@ module.exports = {
                 type: 'asset/resource',
             },
             {
-                test: /\.svg$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: path.join('icons', '[name].[contenthash][ext]'),
-                },
-            },
-            {
                 test: /\.(woff2?|eot|ttf|otf)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: path.join('fonts', '[name].[ext]'),
+                },
             },
         ],
     },
